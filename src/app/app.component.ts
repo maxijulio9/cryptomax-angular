cfcfe7b06dd062d39e77831463ecae60692b9ec9
maxijulio9 <maximilianojavierjulio@gmail.com>
2025-04-18 20:13:33 -0300

    WIP:Adding companies-carrousel component and  Migrating structure and some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from "./components/footer/footer.component";
 import { BodyComponent } from "./components/body/body.component";
+import { CompaniesCarrouselComponent } from './components/companies-carrousel/companies-carrousel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, BodyComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, BodyComponent, CompaniesCarrouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'crypto-max';
 }
---
+++ src/app/components/companies-carrousel/companies-carrousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-companies-carrousel',
+  imports: [],
+  templateUrl: './companies-carrousel.component.html',
+  styleUrl: './companies-carrousel.component.css'
+})
+export class CompaniesCarrouselComponent {
+
+}
