d1a94a661db953ec9b713c2fe62a7caf56793ed0
maxijulio9 <maximilianojavierjulio@gmail.com>
2025-06-10 22:01:42 -0300

    Improve: adding charts from TradingView mini widget- IN REVIEW

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { CriptoPricesComponent } from './components/cripto-prices/cripto-prices.component';
 import { HelpComponent } from './components/help/help.component';
+import { ChartComponent } from './components/charts/charts.component';
 
 
 export const routes: Routes = [
@@ -11,6 +12,7 @@ export const routes: Routes = [
     {path: 'portfolio-react', component: PortfolioReactComponent},
     {path:'portfolio', component: PortfolioComponent},
     {path: 'cripto-prices',component: CriptoPricesComponent},
-    {path: 'help', component: HelpComponent}
+    {path: 'help', component: HelpComponent},
+    {path: 'charts', component: ChartComponent}
 
 ];
